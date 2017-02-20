be382108d718c70f425c9a50911e18af0baed83a
Yaroslav Solonnikov <yaroslav.solonnikov@gmail.com>
2017-02-20 08:26:15 +0200

    minimal ngModule

---
+++ src/main.js
@@ -0,0 +1,5 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_module_0_1 = require('./app/app.module.0');
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_0_1.AppModule);
+//# sourceMappingURL=main.js.map
